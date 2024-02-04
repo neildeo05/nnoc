a4cdcb7428425cce3b105161b9477806839280b7
neildeo05 <neilhdeo@gmail.com>
2024-02-04 10:45:43 -0600

    op graph

--- sw/tiled_network.py
+++ sw/tiled_network.py
@@ -1,23 +1,20 @@
 import torch
 import pprint
 import torch.nn as nn
-from torch.fx import symbolic_trace
 import numpy as np
+from collections import namedtuple
 from TiledTensor import TiledTensor
 
-tile_graph = []
+op_graph = []
 # Simple Example
 def tile_compute(a, b):
     return a @ b
-def tile_accum(a, b, tile_dict, tile_idx):
-    if tile_dict.get(tile_idx[0]):
-        tile_dict[tile_idx[0]].append(tile_idx[1:])
-    else:
-        tile_dict[tile_idx[0]] = [tile_idx[1:]]
+def tile_accum(a, b, tile_idx):
+    Accumulate = namedtuple('Accumulate', ['dest','x_tile','weight_idx'])
+    op_graph.append(Accumulate(*tile_idx))
     return a + b
 
 def tiled_matmul(a, b):
-    tile_dict = {}
     ta = TiledTensor(a.detach().numpy()).tileit()
     tb = TiledTensor(b.detach().numpy()).tileit()
     ra = ta.shape[0]
@@ -29,9 +26,9 @@ def tiled_matmul(a, b):
             for k in range(ca):
                 tile_a = ta[i,k]
                 tile_b = tb[k,j]
-                out[i][j] = tile_accum(out[i][j], tile_compute(tile_a, tile_b), tile_dict, ((i,j), (i,k), (k,j)))
+                out[i][j] = tile_accum(out[i][j], tile_compute(tile_a, tile_b), ((i,j), (i,k), (k,j)))
     out = torch.tensor(np.hstack(np.hstack(out)), dtype=torch.float32)
-    tile_graph.append(tile_dict)
+    op_graph.append('END')
     return out
 
 
@@ -39,16 +36,15 @@ class TiledLinear(nn.Linear):
     def forward(self, x):
         out = tiled_matmul(x, self.weight.t())
         check = torch.matmul(x, self.weight.t())
-        print(out.shape)
         assert(torch.allclose(out, check))
         return out
 
 class MiniModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = TiledLinear(8,24, dtype=torch.float32, bias=False)
+        self.l1 = TiledLinear(4,12,dtype=torch.float32, bias=False)
         self.g1 = nn.GELU()
-        self.l2 = TiledLinear(24,12, dtype=torch.float32, bias=False)
+        self.l2 = TiledLinear(12,4, dtype=torch.float32, bias=False)
 
     def forward(self, x):
         x = self.l1(x)
@@ -57,13 +53,12 @@ class MiniModel(torch.nn.Module):
         return x
 
 
-model = MiniModel()
-# batch dimension of 4
-inp = torch.randn((4,8), dtype=torch.float32)
 
+model = MiniModel()
+inp = torch.randn((4,4), dtype=torch.float32)
 model(inp) # call model
 
-for i in tile_graph:
-    pprint.pprint(i)
-    print()
 
+
+for i in op_graph:
+    print(i)
